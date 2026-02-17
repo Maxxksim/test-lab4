6a651fd4b3139c1fbf0c50c654f16eba56bfb7ad
Maksym <makximchagin@gmail.com>
2026-02-17 13:12:08 +0200

    add credentials + method to get sqs

--- services/db.py
+++ services/db.py
@@ -7,6 +7,6 @@ def get_dynamodb_resource():
         "dynamodb",
         endpoint_url=AWS_ENDPOINT_URL,
         region_name=AWS_REGION,
-        aws_access_key_id="dummy",
-        aws_secret_access_key="dummy",
+        aws_access_key_id="test",
+        aws_secret_access_key="test",
     )
